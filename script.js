8975704da542d2e42195991dbf5ccb1b0219ac07
Abdulhafiz-juhar <abdulhafizjuharajmbest@gmail.com>
2023-08-10 09:05:49 +0300

    Create getComputerChoice() function to make the computer choose

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
- 
-console.log("hello world");
+let getComputerChoice = () => {
+    let rand1to3 = Math.floor(Math.random() * 3) + 1;
+    let computerGuess;
+
+    if (rand1to3 === 1) {
+        computerGuess = 'rock';
+    }
+    else if (rand1to3 === 2) {
+        computerGuess = 'paper';
+    }
+    else if (rand1to3 ===3) {
+        computerGuess = 'scissor';
+    }
+
+    return computerGuess;
+}
+let computerSelection = getComputerChoice();
